e18f35a26304b44bccb1282312d778f9564c8976
sukhumi <lubenkaraslavov@abv.bg>
2022-11-11 17:53:30 +0200

    Fix MetricPrefix coming from a different library after update

--- src/main/java/com/useekm/geosparql/UnitsOfMeasure.java
+++ src/main/java/com/useekm/geosparql/UnitsOfMeasure.java
@@ -1,11 +1,11 @@
 /*
  * Copyright 2013 by TalkingTrends (Amsterdam, The Netherlands)
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://opensahara.com/licenses/apache-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -17,8 +17,8 @@ import org.eclipse.rdf4j.model.IRI;
 import org.eclipse.rdf4j.model.Value;
 import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
 import si.uom.SI;
-import tec.uom.se.unit.MetricPrefix;
 
+import javax.measure.MetricPrefix;
 import javax.measure.quantity.Length;
 import javax.measure.Unit;
 import java.util.HashMap;
